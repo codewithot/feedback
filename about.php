4c842e0844d9c4ec284d12dbf1f9ff3524869d13
victor-devv <vheecthurikuomola@gmail.com>
2022-05-17 07:13:49 +0100

    initial commit

---
+++ about.php
@@ -0,0 +1,7 @@
+<?php include 'inc/header.php'; ?>
+    <img src="/img/1.png" class="w-25 mb-3" alt="">
+    <h2>About</h2>
+
+    <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?</p>
+    
+<?php include 'inc/footer.php'; ?>
